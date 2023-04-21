68c62e7ff740d2a1345de8e1a1bfb0a8b2982e52
feveja <128820930+feveja@users.noreply.github.com>
2023-04-21 12:43:53 -0400

    primer commit lab

---
+++ Laboratorio/LabP1.py
@@ -0,0 +1,19 @@
+import random as rp
+#Para las 2 primeras matrices
+filas = int(input("Ingrese la cantidad filas: "))
+columnas = int(input("Ingrese la cantidad columnas: "))
+#llenamos la matriz con elementos aleatorios 
+M1= [[rp.randint(0,5) for j in range(columnas) for i in range(filas)]]
+M2 = [[rp.randint(0,5) for j in range(columnas) for i in range(filas)]]
+#Para la suma de las matrices M1 y M2
+for i in range(filas):
+    for j in range(columnas):
+        Msuma = M1 + M2
+#Pedimos la tercera matriz
+filas2 = int(input("Ingrese la cantidad filas: "))
+columnas2 = int(input("Ingrese la cantidad columnas: "))
+M3 = [[rp.randint(0,5) for j in range(columnas2) for i in range(filas2)]]
+#Para la resta de la Msuma con M3
+for i in range(filas2):
+    for j in range(columnas2):
+        M3= 
