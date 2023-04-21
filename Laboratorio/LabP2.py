70d722cc292d30a53bc7852d6c452df2a0794ee2
feveja <128820930+feveja@users.noreply.github.com>
2023-04-21 13:19:05 -0400

    pq no me a mandado uno commit

--- Laboratorio/LabP2.py
+++ Laboratorio/LabP2.py
@@ -4,12 +4,12 @@ filas = int(input("Ingrese la cantidad filas: "))
 columnas = int(input("Ingrese la cantidad columnas: "))
 escalar = int(input("Ingrese escalar a multiplicar"))
 #llenamos la matriz con elementos aleatorios 
-M1= [[rp.randint(0,10) for j in range(columnas) for i in range(filas)]]
+M5= [[rp.randint(0,10) for j in range(columnas) for i in range(filas)]]
 #Para la multiplicacion por el escalar 
 for i in range(filas):
     for j in range(columnas):
-        M1 = escalar*M1[i][j] #Segun mi logica M4 seria el resultado de la multiplicacion del escalar con la matriz dando M4 
+        M5 = M5*escalar #Segun mi logica M4 seria el resultado de la multiplicacion del escalar con la matriz dando M4 
 #Para imprimir la matriz resultante 
 for i in range(filas):
     for j in range(columnas):
-        print(M1[filas][columnas]) #Me lanza error al imprimir la matriz resultante
+        print(M5[i][j]) #Me lanza error al imprimir la matriz resultante
