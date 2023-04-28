0796b7e1a058c7eaf30d5c711f851f2419ad27d8
feveja <128820930+feveja@users.noreply.github.com>
2023-04-28 11:14:32 -0400

    Update Lab1.1S1.py

--- Laboratorio/Laboratorio Seccion 1/Lab1.1S1.py
+++ Laboratorio/Laboratorio Seccion 1/Lab1.1S1.py
@@ -1,27 +1,24 @@
 import numpy
-
-print("Creación de matriz 1")
+#Anais Caro
+#Felipe Vera
 filas = int(input("Ingrese la cantidad de filas: "))
 columnas = int(input("Ingrese la cantidad de columnas: "))
 
-m = numpy.zeros((filas, columnas))
+m = numpy.zeros((filas, columnas)) #Creamos una array con 0 para llenarlos
 for i in range(filas):
     filas = []
     for j in range(columnas):
         m[i][j] = int(input(f"Elemento ({i + 1}, {j + 1}): "))
 
-
-print("\nCreación de matriz 2")
 filas2 = int(input("Ingrese la cantidad de filas: "))
 columnas2 = int(input("Ingrese la cantidad de columnas: "))
-
-m2 = numpy.zeros((filas2, columnas2))
+aux = numpy.zeros((filas2, columnas2))
 for i in range(filas2):
     filas2 = []
     for j in range(columnas2):
-        m2[i][j] = int(input(f"Elemento ({i + 1}, {j + 1}): "))
+        aux[i][j] = int(input(f"Elemento ({i + 1}, {j + 1}): "))
 
-if filas == filas2 and columnas == columnas2:
-    print(f"Suma matrices:\n{m+m2}'")
-    print(f"Resta matrices:\n{m-m2}'")
+if filas == filas2 and columnas == columnas2: #Si esto no se cumple estariamos con una matriz que no se puede sumar
+    print(f"Suma matrices:\n{m+aux}'")
+    print(f"Resta matrices:\n{m-aux}'")
 else: print("Las matrices no son iguales en medida")
