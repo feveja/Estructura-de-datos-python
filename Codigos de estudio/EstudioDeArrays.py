7a969a790c056355b9f69217e592af8a73529b76
feveja <128820930+feveja@users.noreply.github.com>
2023-05-18 00:55:17 -0400

    Me olvide de hacer el push señores

--- Codigos de estudio/EstudioDeArrays.py
+++ Codigos de estudio/EstudioDeArrays.py
@@ -1,27 +1,19 @@
-#Arreglos
-edades= [1,2,3,4,5]
-print(edades) #Imprime el array
-#Posicion (indice)
-#0 = 1
-#1 = 2
-#2 = 3
-#3 = 4
-#4 = 5
-mi_edad = edades[4]
-print("mi edad es"+str(mi_edad))
-edades[4]= 15 #Cambiamos el elemento de la posicion 1
-print(edades)
+def cantidad_de_digitos(numero):
+    return len(str(numero))
 
-#Len (Cantidad de elementos que tiene un arreglo)
-print(str(len(edades)))
+def en_posicion(numero, indice, reversa):
+    numero_str = str(numero)
+    if reversa:
+        numero_str = numero_str[::-1]
+    return int(numero_str[indice])
 
-#Metodo 1 append
-edades.append(1)
+def reemplazar(numero, indice, nuevo, reversa):
+    numero_str = str(numero)
+    if reversa:
+        numero_str = numero_str[::-1]
+    numero_str = numero_str[:indice] + str(nuevo) + numero_str[indice+1:]
+    if reversa:
+        numero_str = numero_str[::-1]
+    return int(numero_str)
 
-#Metodo 2 count
-print(str(edades.count(1)))
-
-#Metodo 3 index
-posicion = edades.index(15)
-print(posicion)
-
+print(reemplazar(1810, 0, 2, False))
